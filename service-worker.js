20f22507f3a11fde307197737ec374d56020c71a
y <ofeenee@gmail.com>
2025-07-01 12:26:12 +0300

    initial polish setup

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,20 @@
+let version = null;
+
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open("prints-cache").then(cache => {
       return cache.addAll([
         "./",
+        "./assets/",
+        "./css/",
+        "./js/",
+        "./assets/icon-1024.png",
+        "./css/reset.css",
+        "./css/global.css",
+        "./css/main.css",
+        "./js/main.js",
         "./index.html",
-        "./manifest.json"
+        "./manifest.json",
       ]);
     })
   );
@@ -12,6 +22,11 @@ self.addEventListener("install", event => {
 });
 
 self.addEventListener("fetch", event => {
+  const url = new URL(event.request.url);
+  if (/version\.json$/gi.test(url.pathname)) {
+    return event.respondWith(fetch(event.requets));
+  }
+
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
